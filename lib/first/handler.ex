0203f392e2216f772e3219c64cfaa0f420373296
Dauren Maskeugaliyev <maskeugalievd@gmail.com>
2024-06-08 20:22:20 +0600

    first pseudo code

--- lib/first/handler.ex
+++ lib/first/handler.ex
@@ -1,35 +1,45 @@
 defmodule First.Handler do
   def handle(request) do
-     request
+    request
       |> parse
-      |> response
+      |> route
+      |> response_format
   end
 
   def parse(request) do
     [method, path, _] =
-      request
-        |> String.split("\n")
-        |> List.first
-        |> String.split(" ")
+    request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+      %{method: method, path: path, resp_body: ""}
   end
 
-  def response(_conv) do
+  def route(%{method: "GET", path: "/pokemons"} = conv) do
+    %{conv | resp_body: "Granbull, Porygon2, Zigzagon"}
+  end
+
+  def response_format(conv) do
     """
     HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Pikachu, Raichi, Esh
+    #{conv.resp_body}
     """
   end
+
 end
 
 request = """
-GET /pockemons HTTP/1.1
-Accept-Language: en-us
-Accept-Encoding: gzip, deflate
-Connection: Keep-Alive
+GET /pokemons HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
 """
 
 response = First.Handler.handle(request)
-IO.puts(response)
+
+IO.puts response
